4798bfd550586a262d5c9bf7c73e843bbf42f404
Sam <samjones3001@gmail.com>
2018-02-05 13:59:14 +0000

    First Files for First App

---
+++ First App/First App/First App/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  First App
+//
+//  Created by Samuel Mark Jones on 2/4/18.
+//  Copyright © 2018 Samuel Mark Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func doButtonTap(_ sender: AnyObject) {
+        print("Button touched!")
+    }
+    
+}
+
